b797ee61200aac8d89e2577906bbda90e79c2943
rachita <rachitakhare15@gmail.com>
2021-11-06 10:36:04 -0400

    fixed edit

--- server/models/books.js
+++ server/models/books.js
@@ -2,11 +2,10 @@ let mongoose = require('mongoose');
 
 // create a model class
 let Book = mongoose.Schema({
-    Title: String,
-    Description: String,
-    Price: Number,
-    Author: String,
-    Genre: String
+    title: String,
+    price: Number,
+    author: String,
+    genre: String
 },
 {
   collection: "books"
